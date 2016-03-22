d03a1590d4e315e762215223a8b77664653dd151
Mario Borna Mjertan <bor.mbm@gmail.com>
2016-03-22 21:35:26 +0100

    bok

---
+++ footer.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * The template for displaying the footer.
+ *
+ * Contains the closing of the #content div and all content after.
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-files/#template-partials
+ *
+ * @package LZS3
+ */
+
+?>
+
+	</div><!-- #content -->
+
+	<footer id="colophon" class="site-footer" role="contentinfo">
+		<div class="site-info">
+			<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'lzs3' ) ); ?>"><?php printf( esc_html__( 'Proudly powered by %s', 'lzs3' ), 'WordPress' ); ?></a>
+			<span class="sep"> | </span>
+			<?php printf( esc_html__( 'Theme: %1$s by %2$s.', 'lzs3' ), 'lzs3', '<a href="http://linuxzasve.com" rel="designer">Linux za sve</a>' ); ?>
+		</div><!-- .site-info -->
+	</footer><!-- #colophon -->
+</div><!-- #page -->
+
+<?php wp_footer(); ?>
+
+</body>
+</html>
